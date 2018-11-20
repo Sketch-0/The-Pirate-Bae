74705c6d8704ad4cbcc4b1ca92e31b26ad423446
SketchyCity <44683754+SketchyCity@users.noreply.github.com>
2018-11-20 16:26:22 -0600

    Add files via upload

---
+++ src/piratePackage/Movie.java
@@ -0,0 +1,110 @@
+package piratePackage;
+
+import java.sql.*;
+import java.awt.List;
+
+public class Movie {    
+    int movieID;
+    String movieGenre;
+    String movieTitle;
+    String movieDescription;
+    int movieYearReleased;
+    String movieImage;
+    String movieTrailer;
+    Date movieReleaseDate;
+    String movieMPAARating;
+    String director;
+    String actor1;
+    String actor2;
+    
+    public Movie(int id) {
+        this.movieID = id;
+    }
+    
+    public Movie(int id,
+    			 String genre,
+    			 String title,
+    			 String description,
+    			 int year,
+    			 String image,
+    			 String trailer,
+    			 Date releaseDate,
+    			 String mpaa,
+    			 String director,
+    			 String actor1,
+    			 String actor2) {
+    	this.movieID = id;
+    	this.movieGenre = genre;
+    	this.movieTitle = title;
+    	this.movieDescription = description;
+    	this.movieYearReleased = year;
+    	this.movieImage = image;
+    	this.movieTrailer = trailer;
+    	this.movieReleaseDate = releaseDate;
+    	this.movieMPAARating = mpaa;
+    	this.director = director;
+    	this.actor1 = actor1;
+    	this.actor2 = actor2;
+    }
+    
+    public Connection getConnection( ){
+        Connection connection = null;        
+        try {
+                Class.forName("com.mysql.jdbc.Driver");			
+                connection = DriverManager.getConnection("jdbc:mysql://localhost/moviestoredb", "root", "password");			
+	}
+        catch (Exception e) {
+                e.printStackTrace();
+        }
+        return connection;
+    }
+    
+    public int getID() {
+        return movieID;
+    }
+    
+    public String getGenre() {
+    	return movieGenre;
+    }
+    
+    public String getTitle() {
+    	return movieTitle;
+    }
+    
+    public String getDescription() {
+    	return movieDescription;
+    }
+    
+    public int getYearReleased() {
+    	return movieYearReleased;
+    }
+    
+    public String getImage() {
+    	return movieImage;
+    }
+    
+    public String getTrailer() {
+    	return movieTrailer;
+    }
+    
+    public Date getReleaseDate() {
+    	return movieReleaseDate;
+    }
+    
+    public String getRating() {
+    	return movieMPAARating;
+    }
+    
+    public String getDirector() {
+    	return director;
+    }
+    
+    public String getActor1() {
+    	return actor1;
+    }
+
+    public String getActor2() {
+    	return actor2;
+    }
+    
+}
--- src/piratePackage/PirateUtility.java
+++ src/piratePackage/PirateUtility.java
@@ -1,9 +1,12 @@
 package piratePackage;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PirateUtility {
     
@@ -72,4 +75,157 @@ public class PirateUtility {
 			e.printStackTrace();
 		}
 	}
+    	
+    public int getFavorites() {
+    	
+    	return -1;
+    }
+    
+    public Movie getMovie(String movieID) {
+    	Movie movieBuffer = null;
+    	try {
+			Class.forName("com.mysql.jdbc.Driver");			
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/moviestoredb", "root", "password");			
+
+        	Statement st = con.createStatement();
+
+        	ResultSet movieResult = st.executeQuery("select * from movie where movieID = '" + movieID + "';");
+        	while(movieResult.next())
+        	{
+        		movieBuffer = new Movie(movieResult.getInt(1),
+        					  movieResult.getString(2),
+        					  movieResult.getString(3),
+        					  movieResult.getString(4),
+        					  movieResult.getInt(5),
+        					  movieResult.getString(6),
+        					  movieResult.getString(7),
+        					  movieResult.getDate(8),
+        					  movieResult.getString(9),
+        					  movieResult.getString(10),
+        					  movieResult.getString(11),
+        					  movieResult.getString(12));
+        	}
+            return movieBuffer;
+        }
+    catch (Exception e) {
+            e.printStackTrace();
+    }
+    return null;
+    }
+    
+    public ArrayList<Movie> getGenre(String genre) {
+    	ArrayList<Movie> movieList = null;
+    	try {
+			Class.forName("com.mysql.jdbc.Driver");			
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/moviestoredb", "root", "password");			
+
+        	Statement st = con.createStatement();
+
+        	ResultSet movieResult = st.executeQuery("select * from movie where movieGenre = '" + genre + "';");
+        	while(movieResult.next())
+        	{
+        		movieList.add(new Movie(movieResult.getInt(1),
+        					  movieResult.getString(2),
+        					  movieResult.getString(3),
+        					  movieResult.getString(4),
+        					  movieResult.getInt(5),
+        					  movieResult.getString(6),
+        					  movieResult.getString(7),
+        					  movieResult.getDate(8),
+        					  movieResult.getString(9),
+        					  movieResult.getString(10),
+        					  movieResult.getString(11),
+        					  movieResult.getString(12)));
+        	}
+            return movieList;
+        }
+    catch (Exception e) {
+            e.printStackTrace();
+    }
+    return null;
+    }
+    
+    public ArrayList<Movie> getFavorites(int memberID) {
+    	ArrayList<Movie> movieList = null;
+    	ArrayList<Integer> movieIDList = null;
+    	try {
+			Class.forName("com.mysql.jdbc.Driver");			
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/moviestoredb", "root", "password");			
+
+        	Statement st = con.createStatement();
+
+        	
+        	ResultSet favoriteIDs = st.executeQuery("select movieID from movie where memberID = '" + memberID + "';");
+        	while(favoriteIDs.next())
+        	{
+        		movieIDList.add(favoriteIDs.getInt(1));
+        	}
+        	for(int i = 0; i < movieIDList.size(); i++) {
+        		ResultSet movieResult = st.executeQuery("select * from movie where movieID = '" + movieIDList.get(i) + "';");
+	        	while(movieResult.next())
+	        	{
+	        		movieList.add(new Movie(movieResult.getInt(1),
+	        					  movieResult.getString(2),
+	        					  movieResult.getString(3),
+	        					  movieResult.getString(4),
+	        					  movieResult.getInt(5),
+	        					  movieResult.getString(6),
+	        					  movieResult.getString(7),
+	        					  movieResult.getDate(8),
+	        					  movieResult.getString(9),
+	        					  movieResult.getString(10),
+	        					  movieResult.getString(11),
+	        					  movieResult.getString(12)));
+	        	}
+        	}
+            return movieList;
+        }
+    	catch (Exception e) {
+            e.printStackTrace();
+    	}
+    	return null;
+    }
+    
+    public int getUserStarRating(String memberID, String movieID) {
+    	try {
+    			Class.forName("com.mysql.jdbc.Driver");			
+    			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/moviestoredb", "root", "password");			
+
+            	Statement st = con.createStatement();
+
+            	ResultSet starValue = st.executeQuery("select ratingScore from starrating where movieID = '" + movieID + "' and memberID = '" + memberID + "';");
+            	starValue.next();
+                return starValue.getInt(1);
+            }
+        catch (Exception e) {
+                e.printStackTrace();
+        }
+        return -1;
+    }
+    
+    public float getMovieStarRating(String movieID) {
+    	List<Integer> ratings = new ArrayList<Integer>();
+    	int ratingTotal = 0;
+    	try {
+    			Class.forName("com.mysql.jdbc.Driver");			
+    			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/moviestoredb", "root", "password");			
+
+            	Statement st = con.createStatement();
+
+            	ResultSet starValue = st.executeQuery("select ratingScore from starrating where movieID = '" + movieID + "';");
+            	while(starValue.next())
+            	{
+            		ratings.add(starValue.getInt(1));
+            	}
+            	for(int i = 0; i < ratings.size(); i++) {
+            		ratingTotal += ratings.get(i);
+            	}
+                return ratingTotal / ratings.size();
+            }
+        catch (Exception e) {
+                e.printStackTrace();
+        }
+        return -1;
+    }
+    
 }
